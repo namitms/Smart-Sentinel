5a87c2a7093202562b28fe90db808170d45e359d
namitms <namit.t@microsoft.com>
2020-09-11 12:30:46 +1000

    Authentication Attribute for APIKey based authentication

---
+++ Code/Thanos.Sentinel/Thanos.Sentinel/Helpers/ConstantStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Thanos.Sentinel.Helpers
+{
+    public static class ConstantStrings
+    {
+        public const string APIKEY = "APIKey";
+    }
+}
