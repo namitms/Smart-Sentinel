690bb311e37c778235a89f25f8bb521cb592e6bb
namitms <namit.t@microsoft.com>
2020-09-11 12:10:02 +1000

    Initial Solution Structure
    scaffolding for the server solution with ASP.net Core API controllers

---
+++ Code/Thanos.Sentinel/Thanos.Sentinel/Controllers/TaskController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Thanos.Sentinel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "It will roll now";
+        }
+    }
+}
